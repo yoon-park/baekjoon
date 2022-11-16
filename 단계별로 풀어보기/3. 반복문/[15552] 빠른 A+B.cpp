ccf33444f1e17c576a22da26e0aa3550673ace1c
yoon-park <jiyoonpark.dev@gmail.com>
2022-11-17 00:25:39 +0900

    22.11.16

---
+++ 단계별로 풀어보기/3. 반복문/[15552] 빠른 A+B.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	int t, a, b;
+	scanf("%d", &t);
+
+	for (int i = 0; i < t; i++)
+	{
+		scanf("%d %d", &a, &b);
+		printf("%d \n", a + b);
+	}
+	
+	return 0;
+}
