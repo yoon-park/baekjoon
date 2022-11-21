38acd031769459f46c116c7e0a612cd18cf64052
yoon-park <jiyoonpark.dev@gmail.com>
2022-11-21 16:03:09 +0900

    22.11.21

---
+++ 단계별로 풀어보기/3. 반복문/[10952] A+B - 5.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+ 
+ 	while (true)
+	{
+		scanf("%d %d", &a, &b);
+
+		if (a == 0 & b == 0)	break;
+		else	printf("%d\n", a + b);
+	}
+
+	return 0;
+}
